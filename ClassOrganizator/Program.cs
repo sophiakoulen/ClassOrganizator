9c7d431b437981aceba7d150f72d4020e8ac1765
Sophia Koulen <s.koulen@rollomatic.ch>
2023-11-03 11:52:15 +0100

    Add project files.

---
+++ ClassOrganizator/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Welcome to Class Organizator");
+
+/* Actions:
+ * - create a class
+ * - create a student person
+ * - create a teacher person
+ * - assign a person to a class
+ * - remove a person from a class
+ * - see all classes
+ * - see one class
+ * - delete a class
+ * - delete a person
