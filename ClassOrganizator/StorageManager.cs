0c5b1e458ae8a505f11dde9aeef92866b64fb6bb
Sophia Koulen <s.koulen@rollomatic.ch>
2023-11-09 16:19:23 +0100

    Add StorageManager but does not compile

---
+++ ClassOrganizator/StorageManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassOrganizator
+{
+    internal class StorageManager
+    {
+        public StorageManager(string filePath)
+        {
+
+        }
+
+        public PersonManager PersonManager { get; private set; } = new PersonManager();
+        public ClassManager ClassManager { get; private set; } = new ClassManager();
+
+        public FavouriteClassManager FavouriteClassManager { get; private set; } = new FavouriteClassManager();
+
+        public void StorageManager.Save()
+        {
+            
+        }
+    }
+}
